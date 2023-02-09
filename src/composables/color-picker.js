c0c6897199b4f36104a1372b03d97a41f6d31906
Daniel Tolani <mumytosin2016@gmail.com>
2023-02-09 13:44:41 +0100

    created color picker component

--- src/composables/color-picker.js
+++ src/composables/color-picker.js
@@ -1,2 +1,12 @@
 import { ref } from 'vue'
 
+const useColorPicker = () => {
+    const message = ref('Pick a color')
+    const colors = ref(['green', 'red', 'blue', 'purple'])
+
+
+
+    return { message, colors, matchColor }
+}
+
+export default useColorPicker
